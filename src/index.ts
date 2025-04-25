4abebf8efed78751010fd1bb994c9a9cc822d373
JuanJoseRodriguezdePraAviles <45094901+JuanJoseRodriguezdePraAviles@users.noreply.github.com>
2025-04-25 15:22:23 +0200

    merging files

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/api/rooms", roomsRouter);
-app.use("/api/bookings", bookingsRouter);
-app.use("/api/reviews", reviewsRouter);
-app.use("/api/employees", employeesRouter);
-app.use("/api/login", loginRouter);
+app.use("/api/v1/rooms", roomsRouter);
+app.use("/api/v1/bookings", bookingsRouter);
+app.use("/api/v1/reviews", reviewsRouter);
+app.use("/api/v1/employees", employeesRouter);
+app.use("/api/v1/login", loginRouter);
 
 app.listen(PORT, () => {
     console.log(`Server listening en http://localhost:${PORT}`)
