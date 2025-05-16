99481718e0fbd8833fb10b530412a7f2417c237a
JuanJoseRodriguezdePraAviles <45094901+JuanJoseRodriguezdePraAviles@users.noreply.github.com>
2025-05-16 07:52:38 +0200

    removed dist from repo

--- dist/interfaces/BookingStatus.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BookingStatus = void 0;
-var BookingStatus;
-(function (BookingStatus) {
-    BookingStatus["CheckIn"] = "Check In";
-    BookingStatus["CheckOut"] = "Check Out";
-    BookingStatus["InProgress"] = "In Progress";
-})(BookingStatus || (exports.BookingStatus = BookingStatus = {}));
--- dist/interfaces/RoomStatus.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RoomStatus = void 0;
-var RoomStatus;
-(function (RoomStatus) {
-    RoomStatus["Booked"] = "Booked";
-    RoomStatus["Available"] = "Available";
-})(RoomStatus || (exports.RoomStatus = RoomStatus = {}));
