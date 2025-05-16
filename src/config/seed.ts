c042c3e5618ff5577668836ddb46a27c46855bac
JuanJoseRodriguezdePraAviles <45094901+JuanJoseRodriguezdePraAviles@users.noreply.github.com>
2025-05-16 15:40:49 +0200

    removed sql query

--- src/config/seed.ts
+++ src/config/seed.ts
@@ -1,62 +1,3 @@
-/*
-DROP TABLE review;
-DROP TABLE booking;
-DROP TABLE room, employee;
-CREATE TABLE IF NOT EXISTS room (
-	ID VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
-    room_name VARCHAR(40) NOT NULL,
-    room_type ENUM('Single Bed', 'Double Bed', 'Double Superior', 'Suite'),
-    room_floor VARCHAR(5),
-    status ENUM('Booked', 'Available'),
-    description VARCHAR(1000),
-    photos VARCHAR(1000),
-    offer BOOLEAN,
-    price INT,
-    discount INT,
-    cancellation_policy VARCHAR(1000),
-    room_amenities ENUM('3 Bed Space', '24 Hours Guard', 'Free Wifi', '2 Bathroom', 'Air Conditioner', 'Television')
-);
-
-CREATE TABLE IF NOT EXISTS booking (
-	ID VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT(UUID()),
-    roomID VARCHAR(40) NOT NULL,
-    FOREIGN KEY (roomID) REFERENCES room(ID),
-    clientID VARCHAR(40) NOT NULL,
-    client_name VARCHAR(100),
-    client_email VARCHAR(100),
-    client_phone VARCHAR(40),
-    order_date DATE,
-    check_in_date DATE,
-    check_out_date DATE,
-    status ENUM('Check In', 'Check Out', 'In Progress'),
-    special_request VARCHAR(1000)
-);
-
-CREATE TABLE IF NOT EXISTS review (
-	ID VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT(UUID()),
-    email VARCHAR(40) NOT NULL,
-    date DATE NOT NULL,
-    customer_name VARCHAR(50) NOT NULL,
-    phone VARCHAR(40) NOT NULL,
-    subject VARCHAR(40) NOT NULL,
-    comment VARCHAR(1000) NOT NULL,
-    archived BOOLEAN NOT NULL,
-    clientID VARCHAR(40) NOT NULL,
-    FOREIGN KEY (clientID) REFERENCES booking(ID)
-);
-
-CREATE TABLE IF NOT EXISTS employee (
-	DNI VARCHAR(36) NOT NULL PRIMARY KEY DEFAULT(UUID()),
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    job_functions VARCHAR(1000),
-    registration_date DATE,
-    phone VARCHAR(40),
-    schelude VARCHAR(100),
-    status BOOLEAN
-);
-*/
 import dotenv from 'dotenv';
 import { rooms } from './fake.rooms';
 import { createBookingsForRooms, getRoomIDs } from './fake.bookings';
@@ -98,7 +39,7 @@ export async function seed() {
             );
         }
         console.log(`${bookings.length} inserted bookings`);
-        
+
         const reviews = await createReviewsForBookings();
         for(const review of reviews) {
             await sequelize.query(
